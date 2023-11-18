4abfafe63173de898f4897d0bdb13c8af271809b
Fronend-React-JS <yaxyobeknomonov5@gmail.com>
2023-11-18 07:39:00 +0500

    full error

--- js/script.js
+++ js/script.js
@@ -1,85 +1,127 @@
-import {form, nameForm, yearForm, priceForm, colorForm, addBtn, nameFilter, yearFilter, statusFilter, search, tbody} from "./consts.js";
+import {
+  form,
+  nameForm,
+  yearForm,
+  priceForm,
+  colorForm,
+  addBtn,
+  nameFilter,
+  yearFilter,
+  statusFilter,
+  search,
+  tbody,
+} from "./consts.js";
 
 function validate() {
-    if (!nameForm.value) {
-        nameForm.style.outlineColor = 'red';
-        nameForm.focus();
-        return;
-    }
+  if (!nameForm.value) {
+    nameForm.style.outlineColor = "red";
+    nameForm.focus();
+    return;
+  }
 
-    if (!yearForm.value) {
-        yearForm.style.outlineColor = 'red';
-        yearForm.focus();
-        return;
-    }
+  if (!yearForm.value) {
+    yearForm.style.outlineColor = "red";
+    yearForm.focus();
+    return;
+  }
 
-    if (yearForm.value < 1950 || yearForm.value >=  new Date().getFullYear + 1) {
-        alert("Yilni to'g'ri kiriting");
-        yearForm.style.outlineColor = 'red';
-        yearForm.value = '';
-        yearForm.focus();
-        return;
-    }
+  if (yearForm.value < 1950 || yearForm.value >= new Date().getFullYear + 1) {
+    alert("Yilni to'g'ri kiriting");
+    yearForm.style.outlineColor = "red";
+    yearForm.value = "";
+    yearForm.focus();
+    return;
+  }
 
-    if (!colorForm.value) {
-        colorForm.style.outlineColor = 'red';
-        colorForm.focus();
-        return;
-    }
+  if (!colorForm.value) {
+    colorForm.style.outlineColor = "red";
+    colorForm.focus();
+    return;
+  }
 
-    if (!priceForm.value) {
-        priceForm.style.outlineColor = 'red';
-        priceForm.focus();
-        return;
-    }
+  if (!priceForm.value) {
+    priceForm.style.outlineColor = "red";
+    priceForm.focus();
+    return;
+  }
 
-    if (priceForm.value < 100) {
-        alert('Brat xazillashmang');
-        priceForm.style.outlineColor = 'red';
-        priceForm.focus();
-        return;
-    }
+  if (priceForm.value < 100) {
+    alert("Brat xazillashmang");
+    priceForm.style.outlineColor = "red";
+    priceForm.focus();
+    return;
+  }
+}
 
-};
+addBtn && addBtn.addEventListener("click", function () {
+    try {
+      validate();
 
-addBtn && addBtn.addEventListener('click', function() {
-   try {
-    validate();
-
-    let data = JSON.parse(localStorage.getItem('cars')) || [];
-    const car = {
-        id:Date.now(),
+      let data = JSON.parse(localStorage.getItem("cars")) || [];
+      const car = {
+        id: Date.now(),
         name: nameForm.value,
         year: yearForm.value,
         price: priceForm.value,
         color: colorForm.value,
-        status: 'active'
-    };
+        status: "active",
+      };
 
-    data.push(car);
-    form.reset();
-    localStorage.setItem('cars', JSON.stringify(data));
+      data.push(car);
+      form.reset();
+      localStorage.setItem("cars", JSON.stringify(data));
 
-    let tr = createRow(car, data.length -  1);
-    tbody.innerHTML += tr;
+      let tr = createRow(car, data.length - 1);
+      tbody.innerHTML += tr;
+    } catch (error) {
+      alert("xatolik bor");
+      console.log(error);
+    }
+  });
 
-   } catch (error) {
-    alert("xatolik bor")
-    console.log(error);
-   }
-});
+function filtered(item, element) {
+  const data = JSON.parse(localStorage.getItem("cars")) || [];
+  let filteredData = [];
+  if (!element.value) {
+    filteredData = data;
+  } else {
+    if (data.length) {
+      if (item == "name") {
+        filteredData = data.filter((car) => {
+          return car.name == element.value;
+        });
+      } 
+    }
+
+      if (data.length) {
+        if (item == "year") {
+          filteredData = data.filter((car) => {
+            return car.year == element.value;
+          });
+        } 
+    }
+
+        if (data.length) {
+          if (item == "status") {
+            filteredData = data.filter((car) => {
+              return car.status == element.value;
+            });
+          }
+        }
+      }
+    }
 
 function createRow(car, index) {
-    let status;
-    if (car.status == 'active') {
-        status = 'Sotilmagan';
-    }
+  let status;
+  if (car.status == "active") {
+    status = "Sotilmagan";
+  }
 
-    if (car.status == 'inactive') {
-        status = "Sotilgan";
-    }
+  if (car.status == "inactive") {
+    status = "Sotilgan";
+  }
 
-    let tr = `
+  let tr = `
         <tr>
             <td>${index + 1}</td>
             <td>${car.name}</td>
@@ -90,46 +132,67 @@ function createRow(car, index) {
         </tr>
 
     `;
-    return tr;
+  return tr;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const cars = JSON.parse(localStorage.getItem('cars')) || [];
+document.addEventListener("DOMContentLoaded", function () {
+  const cars = JSON.parse(localStorage.getItem("cars")) || [];
 
-    if (cars.length) {
-        let fakeDom = '';
-        cars.forEach((car, index) => {
-           let tr = createRow(car, index);
-            
-            fakeDom += tr 
-        });
-    
-        tbody.innerHTML += fakeDom;
-    
-    }
+  if (cars.length) {
+    let fakeDom = "";
+    cars.forEach((car, index) => {
+      let tr = createRow(car, index);
+
+      fakeDom += tr;
+    });
+
+    tbody.innerHTML += fakeDom;
+  }
 });
 
-nameFilter && nameFilter.addEventListener('change', function() {
-    const data = JSON.parse(localStorage.getItem('cars')) || [];
-    let filteredData = [];
-    if (!this.value) {
-        filteredData = data;
-    } else {
-        if (data.length) {
-            filteredData = data.filter(car => {
-                 return car.name == this.value;
-            })
-        }
-    }
+nameFilter && nameFilter.addEventListener("change", function () {
+    filtered('name', element)
+});
 
+yearFilter && nameFilter.addEventListener("change", function () {
+    filtered('year', element)
+});
+
+statusFilter && nameFilter.addEventListener("change", function () {
+    filtered('status', element)
+});
+
+search && search.addEventListener('input', function () {
+    const data = JSON.parse(localStorage.getItem("cars")) || [];
+   if (!this.value || this.value.length < 1) {
     tbody.innerHTML = '';
-    let filterFakeDom ='';
-    filteredData && filteredData.forEach((car, index)=> {
+    let fakeFindAll = '';
+    data.forEach((car, index) => {
         let tr = createRow(car, index);
-        filterFakeDom += tr;
+        fakeFindAll = tr;
     })
 
-    tbody.innerHTML += filterFakeDom;
-})
+    tbody.innerHTML += fakeFindAll;
+   } 
 
+   if (this.value.length >= 2) {
+    let find = [];
 
+    if (data.length) {
+        find = data.filter(car => {
+            return car.name.toLowerCars().includes(this.value.toLowerCars());
+        })
+    }
+
+    if (!this.value) {
+        tbody.innerHTML = '';
+        let fakeFind = '';
+        data.forEach((car, index) => {
+            let tr = createRow(car, index);
+            fakeFind = tr;
+        })
+    
+        tbody.innerHTML += fakeFind;
+       } 
+   }
+});
